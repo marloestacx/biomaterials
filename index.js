93632a41a7f99bf653b81367f07a6e8551bda760
marloestacx <marloes.tacx@live.nl>
2022-01-12 15:51:34 +0100

    Popup added

--- index.js
+++ index.js
@@ -7,7 +7,32 @@ var diagonal = d3.svg.diagonal().projection(function (d) {
   return [d.y, d.x];
 });
 
-var div = d3.select("#info").attr("class", "info").style("opacity", 0);
+var div = d3.select("#info").style("opacity", 0);
+
+div.on("mouseout", function (d) {
+  div.transition().duration("50").attr("opacity", "1");
+
+  div.transition().duration(500).style("opacity", 0);
+  div.attr("class", "hide");
+});
+
+// Creates sources <svg> element
+const svginfo = d3.select("#shapes");
+
+// var tooltipWithContent = d3.selectAll("#info, #info *");
+// function equalToEventTarget() {
+//   return this == d3.event.target;
+// }
+
+// d3.select("body").on("click", function () {
+//   var outside = tooltipWithContent.filter(equalToEventTarget).empty();
+//   if (d3.select("#info").classed()[0][0].className == "info") {
+//     div.attr("class", "hide");
+//   }
+// });
+
+// const svg = document.querySelector(".svg");
+// const circle = document.querySelector(".circle");
 
 var svg = d3
   .select("body")
@@ -65,13 +90,32 @@ d3.json("data.json", function (error, root) {
     })
     .attr("dy", 3)
     .on("click", function (d) {
-      console.log(d);
-
       d3.select(this).transition().duration("50").attr("opacity", ".85");
       div.transition().duration(200).style("opacity", 1);
-      d3.select("#info");
       d3.select("#name").text(`Name: ${d.name}`);
       d3.select("#functions").text(`Functions: ${d.functions}`);
+      console.log(d);
+      svginfo
+        .append("circle")
+        .attr("cx", 300)
+        .attr("cy", 80)
+        .attr("r", 37.5)
+        .style("fill", function () {
+          if (d.base === "plants") {
+            return "green";
+          } else if (d.base === "animal") {
+            return "red";
+          } else if (d.base === "petrol-based") {
+            return "black";
+          } else if (d.base === "microbial") {
+            return "lightgreen";
+          } else if (d.base === "inorganic") {
+            return "orange";
+          } else if (d.base === "chemical compounds") {
+            return "yellow";
+          }
+        });
+      div.attr("class", "info");
     })
     .style("text-anchor", function (d) {
       return d.children ? "end" : "start";
